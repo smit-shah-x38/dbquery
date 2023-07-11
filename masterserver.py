3b226ac4cf99901c0068778caac2bab09bd1af52
Smit Shah <101961910+smit-shah-GG@users.noreply.github.com>
2023-07-11 16:01:34 +0530

    Master Server made

---
+++ masterserver.py
@@ -0,0 +1,72 @@
+import mysql.connector
+from flask import Flask, request, jsonify
+import langchain
+from langchain.llms import OpenAI
+from langchain import PromptTemplate, LLMChain
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
+
+llm = OpenAI(
+    openai_api_key="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")
+conversation_history = [
+    "You are a helpful assistant that specializes in creating queries for databases. If what the user asks is related to querying a database, return the query in both MongoDB and SQL, otherwise simply return Please ask a relevant question"
+]
+
+# Import the mysql-connector-python module
+
+# Create a connection object
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root"
+)
+
+
+@app.route("/ask", methods=["POST"])
+@cross_origin()
+def ask():
+    global conversation_history
+    question = request.json["question"]
+    prompt = PromptTemplate(
+        template="\n".join(conversation_history) + "\nQ: {question}\n A: ",
+        input_variables=["question"],
+    )
+
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    response = llm_chain.run(question)
+    conversation_history.append(f"Q: {question}\nA: {response}")
+
+    return jsonify({"response": response})
+
+
+@app.route("/query", methods=["POST"])
+@cross_origin()
+def query():
+
+    qry = request.json["query"]
+    # Create a cursor object
+    mycursor = mydb.cursor()
+
+    # Execute a query to create a database
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS exampledb")
+
+    # Execute a query to use the database
+    mycursor.execute("USE exampledb")
+
+    # Execute a query to select all data
+    mycursor.execute(str(qry))
+
+    # Fetch all the rows from the result set
+    myresult = mycursor.fetchall()
+
+    # Close the cursor and connection objects
+    mycursor.close()
+    mydb.close()
+
+    return jsonify({"response": myresult})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050)
--- sqlqueryrunnerinator.py
+++ sqlqueryrunnerinator.py
@@ -16,25 +16,25 @@ mycursor.execute("CREATE DATABASE IF NOT EXISTS exampledb")
 # Execute a query to use the database
 mycursor.execute("USE exampledb")
 
-# Execute a query to create a table
-mycursor.execute(
-    "CREATE TABLE IF NOT EXISTS customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+# # Execute a query to create a table
+# mycursor.execute(
+#     "CREATE TABLE IF NOT EXISTS customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
 
-# Execute a query to insert some data
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = [
-    ('John', 'Highway 21'),
-    ('Mary', 'Lowstreet 4'),
-    ('Peter', 'Park Lane 38'),
-    ('Amy', 'Apple st 652')
-]
-mycursor.executemany(sql, val)
+# # Execute a query to insert some data
+# sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+# val = [
+#     ('John', 'Highway 21'),
+#     ('Mary', 'Lowstreet 4'),
+#     ('Peter', 'Park Lane 38'),
+#     ('Amy', 'Apple st 652')
+# ]
+# mycursor.executemany(sql, val)
 
-# Commit the changes to the database
-mydb.commit()
+# # Commit the changes to the database
+# mydb.commit()
 
-# Print the number of rows inserted
-print(mycursor.rowcount, "records inserted.")
+# # Print the number of rows inserted
+# print(mycursor.rowcount, "records inserted.")
 
 # Execute a query to select all data
 mycursor.execute("SELECT * FROM customers")
