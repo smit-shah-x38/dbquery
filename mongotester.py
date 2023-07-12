0cd2045e687eb5cab07050f455a565e213da9674
Smit Shah <101961910+smit-shah-GG@users.noreply.github.com>
2023-07-12 18:17:11 +0530

    Scripted

--- mongotester.py
+++ mongotester.py
@@ -1,4 +1,5 @@
 # Import pymongo library
+import pandas as pd
 from pymongo import MongoClient
 
 # Create a MongoClient object with the connection string
@@ -10,7 +11,7 @@ collection = db.accounts
 
 documents = list(collection.find())
 
-print(documents)
+# print(documents)
 
 # invbooks = []
 
@@ -19,3 +20,18 @@ print(documents)
 #         invbooks.append(documents[i])
 
 # print(len(invbooks))
+
+df = pd.DataFrame(documents)
+
+df = df.explode("products")
+
+data = df
+one_hot = pd.get_dummies(data['products'])
+data = data.drop('products', axis=1)
+data = data.join(one_hot)
+
+# assuming data is the DataFrame with one-hot encoded columns
+data2 = data.groupby('_id').any()  # group by ID and apply the any function
+
+counts = data2.sum()  # get the sum of each column
+print(counts)
