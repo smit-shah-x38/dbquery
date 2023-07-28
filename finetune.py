b0b6d41b1990a4e68b663cf3a319fdd051247160
smit-shah-GG <smitshahofficial@gmail.com>
2023-07-28 08:55:32 +0000

    LLM Vicuna

--- finetune.py
+++ finetune.py
@@ -1,12 +1,18 @@
-from autotrain import AutoTrain
+# Import the necessary libraries
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
-# create an instance of AutoTrain
-at = AutoTrain()
+# Load the tokenizer and the model
+tokenizer = AutoTokenizer.from_pretrained("meta-ai/llama-2-7b-chat")
+model = AutoModelForCausalLM.from_pretrained("meta-ai/llama-2-7b-chat")
 
-# specify the task, dataset, and output directory
-task = "text_classification"
-dataset = "path/to/your/dataset.csv"
-output_dir = "path/to/your/output/directory"
+# Define the prompt
+prompt = "Hello, this is Llama 2. How are you today?"
 
-# start a training job
-at.train(task=task, dataset=dataset, output_dir=output_dir)
+# Encode the prompt and generate a response
+input_ids = tokenizer.encode(prompt, return_tensors="pt")
+output_ids = model.generate(input_ids, max_length=50, do_sample=True, top_p=0.9)
+
+# Decode the output and print it
+output = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+print(output)
