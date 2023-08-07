1049c4105930601675041b189db9399f1265b716
smit-shah-GG <smitshahofficial@gmail.com>
2023-08-07 10:33:01 +0000

    locally occuring model directory

--- finetune.py
+++ finetune.py
@@ -3,15 +3,15 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
 # Load the tokenizer and the model
-tokenizer = AutoTokenizer.from_pretrained("meta-ai/llama-2-7b-chat")
-model = AutoModelForCausalLM.from_pretrained("meta-ai/llama-2-7b-chat")
+tokenizer = AutoTokenizer.from_pretrained("/var/basefolder_smit/dbq/localdata")
+model = AutoModelForCausalLM.from_pretrained("/var/basefolder_smit/dbq/localdata")
 
 # Define the prompt
-prompt = "Hello, this is Llama 2. How are you today?"
+prompt = 'Describe the charachter of Vito Corleone in the Godfather. Context: Vito Corleone is the main character in Mario Puzo\'s novel, "The Godfather," and is portrayed by Marlon Brando in the 1972 film adaptation directed by Francis Ford Coppola. Vito Corleone, also known as Don Vito, is a powerful and respected Italian-American crime boss who rules over the Corleone crime family with an iron fist. He is a ruthless and cunning leader who will stop at nothing to protect his family and his business.'
 
 # Encode the prompt and generate a response
 input_ids = tokenizer.encode(prompt, return_tensors="pt")
-output_ids = model.generate(input_ids, max_length=50, do_sample=True, top_p=0.9)
+output_ids = model.generate(input_ids, max_length=200, do_sample=True, top_p=0.9)
 
 # Decode the output and print it
 output = tokenizer.decode(output_ids[0], skip_special_tokens=True)
